32a0244768bdbb6b267588a18b29df34cabfbff5
suaram95 <aram/*java2020>
2021-04-16 15:11:39 +0400

    Initial commit

---
+++ src/test/java/com/example/goodjobtelegrambot/GoodJobTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodjobtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodJobTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
